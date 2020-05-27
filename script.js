9a7ed8599cc60ef05a14cd9a897b9d3f20dad677
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-05-27 10:23:34 -0400

    Set up password length validation and user prompts

--- script.js
+++ script.js
@@ -26,4 +26,29 @@ function generatePassword() {
     config.lowercase = confirm("Do you want to include lowercase characters in your generated password?");
     config.numeric = confirm("Do you want to include numeric characters in your generated password?");
     config.special = confirm("Do you want to include special characters in your generated password?");
+
+    // variables and validation of user input to enforce parameters
+    let wordLength = 0;
+    let ans = "NaN";
+  
+    while (!parseInt(ans)) {
+      ans = parseInt(prompt("How many characters would you like you password to be?"));
+      if (!ans) {
+        alert("The password length must be a number");
+      }
+      while (ans < 8 || ans > 128) {
+        if (ans < 8) {
+          ans = 'NaN';
+          alert("Password length cannot be shorter than 8 characters.")
+        } else if (ans > 128) {
+          ans = 'NaN';
+          alert("Password length cannot exceed 128 characters")
+        }
+      }
+    }
+  
+    wordLength = Math.floor(ans);
+    config.length = wordLength;
+    console.log(config);
+
 }  
