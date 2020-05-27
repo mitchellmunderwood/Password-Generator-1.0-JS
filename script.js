527e37373baa542f25a29728bb0d4b4279928e59
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-05-27 10:15:16 -0400

    Set up initial config object and user input for password

---
+++ script.js
@@ -0,0 +1,29 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+
+function generatePassword() {
+    // config object to store user responses and password configurations
+    const config = {
+      uppercase: false, 
+      lowercase: false, 
+      numeric: false,
+      special: false,
+      length: 0
+    }
+
+    // user prompts to get password configs
+    config.uppercase = confirm("Do you want to include uppercase characters in your generated password?");
+    config.lowercase = confirm("Do you want to include lowercase characters in your generated password?");
+    config.numeric = confirm("Do you want to include numeric characters in your generated password?");
+    config.special = confirm("Do you want to include special characters in your generated password?");
+}  
