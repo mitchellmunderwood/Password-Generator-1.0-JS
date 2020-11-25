dc74096b6bebdced40e2f94578c9456dd8aa658a
Mitchell Underwood <mitchellmunderwood@gmail.com>
2020-11-25 18:07:47 -0500

    Updated Readme

--- README.md
+++ README.md
@@ -1,19 +1,139 @@
-# Password Generator
+<h1 align="center">
+  <br>
+  <!-- image/logo link -->
+  <a href="https://mitchellmunderwood.github.io/Password-Generator-1.0-JS/"><img src="./README-support/logo.png" alt="title" width="200" height="200" ></a>
+  
+  Password Generator 1.0 JS
+  <br>
+</h1>
 
-## Description
+<h4 align="center">
+<!-- Description of Project -->
+Stay safe with secure passwords.
+</h4>
 
-> Here is a password generator for the security conscious individual looking to curate a strong password. The simple pages walks the user through a series of prompts to specify the type of characters the user would like in the password, as well as the password length. Then, the program will automatically generate a password to the users specifications. 
+<p align="center">
+    <!-- table of contents -->
+  <a href="#live">Live</a> •
+  <a href="#gallery">Gallery</a> •
+  <a href="#description">Description</a> •
+  <a href="#license">License</a> •
+  <a href="#stack">Stack</a> •
+  <a href="#installation">Installation</a> •
+  <a href="#usage">Usage</a> •
+  <a href="#testing">Testing</a> •
+  <a href="#questions">Questions</a> •
+  <a href="#contributing">Contributing</a> 
+</p>
 
-## Deployment
+<br>
 
-> Merely open the index.html file in any brower for immediate functionality. Also deployable to Github pages. 
+<p>
+<br>
+<br>
+<br>
+<br>
+<br>
 
-## Features
+</p>
 
-> Allows the user to configure the password to inlude the following: Upper Case Letters, Lower Case Letters, Special Characters, and Numerica Characters. Also allows the user to designate the password length on a scale of 8 to 128 characters. 
+### Live
 
-## Images
+Password Generator is currently deployed with Github. <a href="https://mitchellmunderwood.github.io/Password-Generator-1.0-JS/">Go to Live</a>.
+<br/>
 
-> ![image](./readme/image.png)
->
-> 
+### Gallery
+
+<p align="center">
+    <img src="./README-support/img1.png" width="auto" height="auto">
+</p>
+
+<br/>
+
+### Description
+
+A password generator for the security conscious individual looking to curate a strong password. The simple pages walks the user through a series of prompts to specify the type of characters the user would like in the password, as well as the password length. Then, the program will automatically generate a password to the users specifications.
+
+<br/>
+
+### License
+
+<p>
+<img src="https://img.shields.io/badge/License-MIT-informational?style=flat&logo=mit&logoColor=white&color=blue"
+         alt="MIT License">
+</p>
+
+Password Generator is licensed under the terms of the [MIT License](https://opensource.org/licenses/MIT) and is available for free.
+
+<br/>
+
+### Stack
+
+<p align="left">
+<!-- line for badges -->
+    <img src="https://img.shields.io/badge/Code-HTML5-informational?style=flat&logo=html5&logoColor=white&color=blue"
+         alt="HTML5">
+    <img src="https://img.shields.io/badge/Code-CSS3-informational?style=flat&logo=css3&logoColor=white&color=blue"
+         alt="CSS3">
+    <img src="https://img.shields.io/badge/Code-JavaScript-informational?style=flat&logo=javascript&logoColor=white&color=blue"
+         alt="JavaSript">
+    <!-- <img src="https://img.shields.io/badge/Code-Handlebars-informational?style=flat&logo=handlebars&logoColor=white&color=blue"
+         alt="Express"> -->
+    <!-- <img src="https://img.shields.io/badge/Code-Express-informational?style=flat&logo=express&logoColor=white&color=blue"
+         alt="Express"> -->
+    <!-- <img src="https://img.shields.io/badge/Code-Node-informational?style=flat&logo=node&logoColor=white&color=blue"
+         alt="Node"> -->
+    <!-- <img src="https://img.shields.io/badge/Data-mySQL-informational?style=flat&logo=mysql&logoColor=white&color=blue"
+         alt="Node"> -->
+</p>
+
+<!-- Open Source Tools:
+
+- [Thing](https://thing.com) -->
+
+<br/>
+
+### Installation
+
+Clone the repo.
+
+<br/>
+
+### Usage
+
+Open the html file in the browser.
+
+<br/>
+
+### Questions
+
+For additional questions, email Mitchell Underwood at **mitchellmunderwood@gmail.com**.
+
+<br/>
+
+### Contributing
+
+Want to contribute? Great!
+To fix a bug or enhance an existing feature, follow these steps:
+
+- Fork the repo
+- Create a new branch `git checkout -b improve-feature`
+- Make the appropriate changes in the files
+- Commit your changes `git commit -am 'Improve feature'`
+- Push to the branch `git push origin improve-feature`
+- Create a Pull Request
+
+<br/>
+
+### Contributors
+
+<p align="center">
+<img src="./README-support/mitchell.jpg" width="150" height="150" />
+<br/>
+Mitchell Underwood
+<br/>
+<a href="https://github.com/mitchellmunderwood">Github</a> •
+<a href="https://www.linkedin.com/in/mitchellmunderwood/">LinkedIn</a> •
+<a href="https://whoismitchell.com">Porfolio</a>
+
+</p>
--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
+  console.log(event);
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
@@ -12,105 +13,190 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 function generatePassword() {
-    // config object to store user responses and password configurations
-    const config = {
-      uppercase: false, 
-      lowercase: false, 
-      numeric: false,
-      special: false,
-      length: 0
+  // config object to store user responses and password configurations
+  const config = {
+    uppercase: false,
+    lowercase: false,
+    numeric: false,
+    special: false,
+    length: 0,
+  };
+
+  // variables and validation of user input to enforce parameters
+  let wordLength = 0;
+  let ans = "NaN";
+
+  while (!parseInt(ans)) {
+    ans = parseInt(
+      prompt("How many characters would you like you password to be?")
+    );
+    if (!ans) {
+      alert("The password length must be a number");
     }
+    while (ans < 8 || ans > 128) {
+      if (ans < 8) {
+        ans = "NaN";
+        alert("Password length cannot be shorter than 8 characters.");
+      } else if (ans > 128) {
+        ans = "NaN";
+        alert("Password length cannot exceed 128 characters");
+      }
+    }
+  }
 
-     // variables and validation of user input to enforce parameters
-     let wordLength = 0;
-     let ans = "NaN";
-   
-     while (!parseInt(ans)) {
-       ans = parseInt(prompt("How many characters would you like you password to be?"));
-       if (!ans) {
-         alert("The password length must be a number");
-       }
-       while (ans < 8 || ans > 128) {
-         if (ans < 8) {
-           ans = 'NaN';
-           alert("Password length cannot be shorter than 8 characters.")
-         } else if (ans > 128) {
-           ans = 'NaN';
-           alert("Password length cannot exceed 128 characters")
-         }
-       }
-     }
-   
-     wordLength = Math.floor(ans);
-     config.length = wordLength;
-     console.log(config);
+  wordLength = Math.floor(ans);
+  config.length = wordLength;
+  console.log(config);
 
-    // user prompts to get password configs and validation to ensure at least 1 type of character is selected
+  // user prompts to get password configs and validation to ensure at least 1 type of character is selected
 
-    let no_char = false;
+  let no_char = false;
 
-    while (!no_char) {
-    config.uppercase = confirm("Do you want to include uppercase characters in your generated password?");
-    config.lowercase = confirm("Do you want to include lowercase characters in your generated password?");
-    config.numeric = confirm("Do you want to include numeric characters in your generated password?");
-    config.special = confirm("Do you want to include special characters in your generated password?");
-    no_char = (config.uppercase || config.lowercase || config.numeric || config.special)
+  while (!no_char) {
+    config.uppercase = confirm(
+      "Do you want to include uppercase characters in your generated password?"
+    );
+    config.lowercase = confirm(
+      "Do you want to include lowercase characters in your generated password?"
+    );
+    config.numeric = confirm(
+      "Do you want to include numeric characters in your generated password?"
+    );
+    config.special = confirm(
+      "Do you want to include special characters in your generated password?"
+    );
+    no_char =
+      config.uppercase || config.lowercase || config.numeric || config.special;
     if (!no_char) {
-      alert("you have to select at least one type of character to generate a password")
+      alert(
+        "you have to select at least one type of character to generate a password"
+      );
     }
+  }
+
+  let myPassword = [];
+
+  // arrays for password characters
+  const upperCaseCharacters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const lowerCaseCharacters = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const numericCharacters = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+  const specialCharacters = [
+    "!",
+    "@",
+    "#",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "|",
+    "?",
+    ".",
+    "~",
+  ];
+
+  // loop to generate password of given length, based on config properties
+  while (myPassword.length !== config.length) {
+    if (config.uppercase) {
+      let randomNum = Math.floor(Math.random() * upperCaseCharacters.length);
+      myPassword.push(upperCaseCharacters[randomNum]);
+      if (myPassword.length === config.length) {
+        break;
+      }
     }
-
-    let myPassword = [];
-
-    // arrays for password characters
-    const upperCaseCharacters = ["A", "B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    const lowerCaseCharacters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-    const numericCharacters = [1,2,3,4,5,6,7,8,9,0];
-    const specialCharacters = ["!","@","#","$","%","^","&","*","|","?",".","~"];
-  
-    // loop to generate password of given length, based on config properties
-    while (myPassword.length !== config.length) {
-      if (config.uppercase) {
-        let randomNum = Math.floor(Math.random() * (upperCaseCharacters.length));
-        myPassword.push(upperCaseCharacters[randomNum]);
-        if (myPassword.length === config.length) {break;}
+    if (config.lowercase) {
+      let randomNum = Math.floor(Math.random() * lowerCaseCharacters.length);
+      myPassword.push(lowerCaseCharacters[randomNum]);
+      if (myPassword.length === config.length) {
+        break;
       }
-      if (config.lowercase) {
-        let randomNum = Math.floor(Math.random() * (lowerCaseCharacters.length));
-        myPassword.push(lowerCaseCharacters[randomNum]);
-        if (myPassword.length === config.length) {break;}
-      }
-      if (config.numeric) {
-        let randomNum = Math.floor(Math.random() * (numericCharacters.length));
-        myPassword.push(numericCharacters[randomNum]);
-        if (myPassword.length === config.length) {break;}
-      }
-      if (config.special) {
-        let randomNum = Math.floor(Math.random() * (specialCharacters.length));
-        myPassword.push(specialCharacters[randomNum]);
-        if (myPassword.length === config.length) {break;}
-      }
-  
     }
-
-    // randomize order of characters
-    
-    let shuffleNum = myPassword.length;
-    while (shuffleNum > 0) {
-      let randomIndex = Math.floor(Math.random() * shuffleNum);
-      shuffleNum = shuffleNum - 1;
-      let tempChar = myPassword[shuffleNum];
-      myPassword[shuffleNum] = myPassword[randomIndex];
-      myPassword[randomIndex] = tempChar;
+    if (config.numeric) {
+      let randomNum = Math.floor(Math.random() * numericCharacters.length);
+      myPassword.push(numericCharacters[randomNum]);
+      if (myPassword.length === config.length) {
+        break;
+      }
     }
+    if (config.special) {
+      let randomNum = Math.floor(Math.random() * specialCharacters.length);
+      myPassword.push(specialCharacters[randomNum]);
+      if (myPassword.length === config.length) {
+        break;
+      }
+    }
+  }
 
-    myPassword = myPassword.join("");
+  // randomize order of characters
 
-  
-    console.log('Password', myPassword);
-    console.log('Password Length', myPassword.length);
+  let shuffleNum = myPassword.length;
+  while (shuffleNum > 0) {
+    let randomIndex = Math.floor(Math.random() * shuffleNum);
+    shuffleNum = shuffleNum - 1;
+    let tempChar = myPassword[shuffleNum];
+    myPassword[shuffleNum] = myPassword[randomIndex];
+    myPassword[randomIndex] = tempChar;
+  }
 
-    // password return for function
-    return(myPassword);
+  myPassword = myPassword.join("");
 
-}  
+  console.log("Password", myPassword);
+  console.log("Password Length", myPassword.length);
+
+  // password return for function
+  return myPassword;
+}
